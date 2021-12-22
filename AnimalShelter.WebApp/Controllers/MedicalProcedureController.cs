24a9d54777c2c3d95db3800cc63a6b5a8625b03f
KamilSztandur <kamil.sztandur@vp.pl>
2021-12-22 15:20:40 +0100

    Add MedicalProcedureController

---
+++ AnimalShelter.WebApp/Controllers/MedicalProcedureController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.WebApp.Controllers
+{
+    public class MedicalProcedureController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AnimalShelter.WebApp/Models/MedicalProcedureVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AnimalShelter.WebApp.Models
+{
+    public class MedicalProcedureVM
+    {
+        public int Id { set; get; }
+        public int DoctorId { set; get; }
+        public int AnimalId { set; get; }
+        public string ProcedureName { set; get; }
+        public bool WasSuccess { set; get; }
+        public DateTime Date { set; get; }
+    }
+}
