0b120ef9479dbf117a699f3a3f033a7e3a0dff6b
KamilSztandur <kamil.sztandur@vp.pl>
2021-12-22 13:58:03 +0100

    Add and implement MedicalProcedureController

---
+++ AnimalShelter.WebApi/Controllers/MedicalProcedureController.cs
@@ -0,0 +1,60 @@
+﻿using AnimalShelter.Infrastructure.Commands;
+using AnimalShelter.Infrastructure.DTO;
+using AnimalShelter.Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class MedicalProcedureController : Controller
+    {
+        private readonly IMedicalProcedureService _medicalProcedureService;
+        public MedicalProcedureController(IMedicalProcedureService medicalProcedureService)
+        {
+            _medicalProcedureService = medicalProcedureService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> BrowseAll()
+        {
+            IEnumerable<MedicalProcedureDTO> z = await _medicalProcedureService.BrowseAll();
+
+            return Json(z);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMedicalProcedure(int id)
+        {
+            MedicalProcedureDTO z = await _medicalProcedureService.GetMedicalProcedure(id);
+
+            return Json(z);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddMedicalProcedure([FromBody] CreateMedicalProcedure medicalProcedure)
+        {
+            var result = await _medicalProcedureService.AddMedicalProcedure(medicalProcedure);
+
+            return Json(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateMedicalProcedure([FromBody] CreateMedicalProcedure medicalProcedure, int id)
+        {
+            var result = await _medicalProcedureService.UpdateMedicalProcedure(id, medicalProcedure);
+
+            return Json(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMedicalProcedure(int id)
+        {
+            var result = await _medicalProcedureService.DeleteMedicalProcedure(id);
+
+            return Json(result);
+        }
+    }
+}
