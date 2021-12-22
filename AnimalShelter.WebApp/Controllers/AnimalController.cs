c8edf75d65c3596758901ee8d70ff1892385104a
KamilSztandur <kamil.sztandur@vp.pl>
2021-12-22 14:45:46 +0100

    Add AnimalController

---
+++ AnimalShelter.WebApp/Controllers/AnimalController.cs
@@ -0,0 +1,159 @@
+﻿using AnimalShelter.WebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalShelter.WebApp.Controllers
+{
+    public class AnimalController : Controller
+    {
+        public IConfiguration Configuration;
+
+        public AnimalController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public ContentResult GetHostUrl()
+        {
+            var result = Configuration["RestApiUrl:HostUrl"];
+            return Content(result);
+        }
+
+        private string CN()
+        {
+            string cn = ControllerContext.RouteData.Values["controller"].ToString();
+            return cn;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            List<AnimalVM> animalsList = new List<AnimalVM>();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync(_restpath))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    animalsList = JsonConvert.DeserializeObject<List<AnimalVM>>(apiResponse);
+                }
+            }
+
+            return View(animalsList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(AnimalVM t)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            using (var httpClient = new HttpClient())
+            {
+                string jsonString = System.Text.Json.JsonSerializer.Serialize(t);
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                await httpClient.PostAsync(_restpath, content);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            AnimalVM t = new AnimalVM();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    t = JsonConvert.DeserializeObject<AnimalVM>(apiResponse);
+
+                }
+            }
+
+            return View(t);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(AnimalVM t)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            Boolean result;
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    string jsonString = System.Text.Json.JsonSerializer.Serialize(t);
+                    var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+
+                    using (var response = await httpClient.PutAsync($"{_restpath}/{t.Id}", content))
+                    {
+                        string apiResponse = await response.Content.ReadAsStringAsync();
+                        result = JsonConvert.DeserializeObject<Boolean>(apiResponse);
+
+                        return RedirectToAction(nameof(Index));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return View(ex);
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            AnimalVM t = new AnimalVM();
+
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{_restpath}/{id}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    t = JsonConvert.DeserializeObject<AnimalVM>(apiResponse);
+
+                }
+            }
+
+            return View(t);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(AnimalVM t)
+        {
+            string _restpath = GetHostUrl().Content + CN();
+
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    var response = await httpClient.DeleteAsync($"{_restpath}/{t.Id}");
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+        }
+    }
+}
---
+++ AnimalShelter.WebApp/Models/AnimalVM.cs
@@ -0,0 +1,11 @@
+﻿namespace AnimalShelter.WebApp.Models
+{
+    public class AnimalVM
+    {
+        public int Id { set; get; }
+        public int BoxId { set; get; }
+        public int MainDoctorId { set; get; }
+        public string Name { set; get; }
+        public bool IsReadyForAdoption { set; get; }
+    }
+}
